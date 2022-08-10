65b16756cda48fa096d7877aa11cbafb4b6521fe
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-08-10 00:19:38 -0500

    feat: add red social icons

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+      <a
+        href="https://www.facebook.com/jesusleonchavez/"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fab fa-facebook"></i>
+      </a>
+
+      <a
+        href="https://www.instagram.com/leonidas.leon.chavez/?hl=es"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fab fa-instagram"></i>
+      </a>
+
+      <a
+        href="https://twitter.com/RedAlbertJesus"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fab fa-twitter"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/jesus-leon-chavez/"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fab fa-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/JesusLeonChavez"
+        className="home__social-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fab fa-github"></i>
+      </a>
+
+      <a
+        href="https://trailblazer.me/id?lang=es_MX"
+        className="home__social-link"
+        target="_blank" rel="noreferrer"
+      >
+        <i class="fab fa-salesforce"></i>
+      </a>
+    </div>
+  );
+}
+
+export default HeaderSocials

6423f3fc85f93413718f812faed1381c2c50a3a3
LEONIDAS <jesus.leon1@unmsm.edu.pe>
2022-08-10 12:06:26 -0500

    fix: upgrade home component, add HeaderSocial, ScrollDown and shapes components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import "./home.css";
 import Me from "../../assets/avatar-1.svg";
 import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
+import Shapes from './Shapes';
 
 const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
         <img src={Me} alt="" className="home__img" />
-        <h1 className="home__name">Jesus Leon (Leonidas)</h1>
-        <span className="home__education">I'm a Front-end developer and Salesforce developer</span>
+        <h1 className="home__name">Jesus Leon</h1>
+        <span className="home__education">I'm a Front-end developer</span>
 
         <HeaderSocials />
+
+        <a href="#contact" className="btn">Hire me</a>
+
+        <ScrollDown />
       </div>
+
+      <Shapes />
     </section>
   )
 }

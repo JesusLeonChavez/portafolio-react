b699f21ac8dd187a661c919ff1f5c61cb828a94f
LEONIDAS <jesus.leon1@unmsm.edu.pe>
2022-09-13 17:42:11 -0500

    fix: upgrade contact component

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -37,7 +37,7 @@ const SideBar = () => {
             </li>
 
             <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
+              <a href="#work" className="nav__link">
                 <i className="icon-layers"></i>
               </a>
             </li>
